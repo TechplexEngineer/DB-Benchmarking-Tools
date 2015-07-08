b76d24338aad1280a946f600abfcda1a997e1153
Blake Bourque <Techplex.Engineer@gmail.com>
2015-07-08 16:39:09 -0400

    Add human readable option

--- plotting/genstats.py
+++ plotting/genstats.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 
 # This function expects the data in [filename] to be tab separated.
 # Col1 is the unix timestamp and col2 is the database transactions per second
-def genstats(filename, start, end, plot=False, print_dates=False):
+def genstats(filename, start, end, plot=False, print_dates=False, human_readable=True):
 	
 	data =  np.loadtxt(filename, delimiter='\t')
 
@@ -31,12 +31,13 @@ def genstats(filename, start, end, plot=False, print_dates=False):
 	if start < 0:
 		start = 0
 
-	print "start\t\t:", start
-	print "start_date\t:", dates[start]
-	print "end\t\t:", end
-	print "end_date\t:", dates[end]
-	print "elasped\t\t:", (dates[end]-dates[start])
-	print "avg\t\t:", np.average(data[start:end,1])
+	if human_readable:
+		print "start\t\t:", start
+		print "start_date\t:", dates[start]
+		print "end\t\t:", end
+		print "end_date\t:", dates[end]
+		print "elasped\t\t:", (dates[end]-dates[start])
+		print "avg\t\t:", np.average(data[start:end,1])
 
 	print "{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(start,end,dates[start],dates[end],(dates[end]-dates[start]),np.average(data[start:end,1]))
 
@@ -62,8 +63,8 @@ def genstats(filename, start, end, plot=False, print_dates=False):
 if __name__ == "__main__":
 	print "Test Routine"
 
-	genstats("testdata.txt", 0, -1, plot=True, print_dates=True)
+	genstats("testdata.txt", 0, -1, plot=True, print_dates=True, human_readable=True)
 
-	genstats("testdata.txt", 0, -1, plot=True, print_dates=False)
+	genstats("testdata.txt", 0, -1, plot=True, print_dates=False, human_readable=True)
 
-	genstats("testdata.txt", 0, -1, plot=False, print_dates=True)
+	genstats("testdata.txt", 0, -1, plot=False, print_dates=True, human_readable=True)
